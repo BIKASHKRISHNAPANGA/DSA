a409716282b82709eae228e116f87fc9af20b273
bikash krishna panga <developerbikash16@gmail.com>
2025-07-25 00:17:09 +0530

    Check Whether Two Strings are Almost Equivalent

--- Strings/Check Whether Two Strings are Almost Equivalent.js
+++ Strings/Check Whether Two Strings are Almost Equivalent.js
@@ -19,3 +19,24 @@ var checkAlmostEquivalent = function(word1, word2) {
     return true
     
 };
+//take the sme time comblexity 
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var checkAlmostEquivalent = function(word1, word2) {
+    const freq= new Array(26).fill(0);
+    for(let char of word1){
+       freq[char.charCodeAt()-97]++;
+    }
+    for(let char of word2){
+        freq[char.charCodeAt()-97]--;
+    }
+    for(let char of freq){
+        if(Math.abs(char)>3)
+        return false ;
+    }
+    return true;
+    
+};
